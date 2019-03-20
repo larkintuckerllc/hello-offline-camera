58fb9ea47e7a1d7ee0a1106bff988f5bdb7c1d70
John Tucker <jtucker@appirio.com>
2019-03-20 11:37:21 -0400

    Getting Rid of Unused

--- src/components/Status/StatusView/index.js
+++ src/components/Status/StatusView/index.js
@@ -1,23 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Button, Modal, Text, View } from 'react-native';
+import { Button, Modal, View } from 'react-native';
 import styles from './styles';
 
-const StatusView = ({ notification, notificationOff, online, onlineRequested }) => (
+const StatusView = ({ notification, notificationOff }) => (
   <Modal visible={notification}>
     <View style={styles.container}>
-      <Text>
-        Notification:
-        {notification ? 'TRUE' : 'FALSE'}
-      </Text>
-      <Text>
-        Online:
-        {online ? 'TRUE' : 'FALSE'}
-      </Text>
-      <Text>
-        Online Requested:
-        {onlineRequested ? 'TRUE' : 'FALSE'}
-      </Text>
       <Button onPress={notificationOff} title="Acknowledge" />
     </View>
   </Modal>
@@ -26,8 +14,6 @@ const StatusView = ({ notification, notificationOff, online, onlineRequested }) 
 StatusView.propTypes = {
   notification: PropTypes.bool.isRequired,
   notificationOff: PropTypes.func.isRequired,
-  online: PropTypes.bool.isRequired,
-  onlineRequested: PropTypes.bool.isRequired,
 };
 
 export default StatusView;
--- src/components/Status/index.js
+++ src/components/Status/index.js
@@ -1,12 +1,9 @@
 import { connect } from 'react-redux';
 import StatusView from './StatusView';
 import { notificationGet, notificationOff } from '../../ducks/notification';
-import { onlineGet, onlineGetRequested } from '../../ducks/online';
 
 const mapStateToProps = state => ({
   notification: notificationGet(state),
-  online: onlineGet(state),
-  onlineRequested: onlineGetRequested(state),
 });
 
 const mapDispatchToProps = {
