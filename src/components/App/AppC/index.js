162f74f7b744f6ccde0ca9780e4fe84690a393dd
John Tucker <jtucker@appirio.com>
2019-03-20 16:28:08 -0400

    Basic Camera

--- src/components/App/AppC/index.js
+++ src/components/App/AppC/index.js
@@ -1,3 +1,4 @@
+import { ImagePicker, Permissions } from 'expo';
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
 import { Button, Text, View } from 'react-native';
@@ -9,6 +10,15 @@ export default class AppC extends PureComponent {
       navigate: PropTypes.func.isRequired,
     }).isRequired,
   };
+
+  state = {
+    hasCameraPermission: null,
+  };
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
 
   handleAPress = () => {
     const {
@@ -24,12 +34,29 @@ export default class AppC extends PureComponent {
     navigate('AppB');
   };
 
+  handleTakePhotoPress = async () => {
+    const { cancelled, uri } = await ImagePicker.launchCameraAsync({});
+    if (cancelled) {
+      return;
+    }
+    console.log(uri);
+  };
+
   render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return null;
+    }
     return (
       <View style={styles.container}>
         <Button title="A" onPress={this.handleAPress} />
         <Button title="B" onPress={this.handleBPress} />
         <Text>C</Text>
+        {hasCameraPermission ? (
+          <Button title="Take Photo" onPress={this.handleTakePhotoPress} />
+        ) : (
+          <Text>No access to camera</Text>
+        )}
       </View>
     );
   }
