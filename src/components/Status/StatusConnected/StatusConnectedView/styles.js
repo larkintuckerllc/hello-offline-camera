c5798f4bb639fee544dfe8156f275062e928c623
John Tucker <jtucker@appirio.com>
2019-03-22 06:59:18 -0400

    color

--- src/components/Status/StatusConnected/StatusConnectedView/styles.js
+++ src/components/Status/StatusConnected/StatusConnectedView/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ccc',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
